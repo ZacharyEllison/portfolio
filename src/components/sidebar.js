ea6fcea5641ab828e9e284884568b183a0390adf
zacharyellison <zellison@college.harvard.edu>
2020-10-13 09:50:44 -0600

    changed some styling and indentations

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -31,41 +31,41 @@ const theSidebar = () => {
               }}
             />
           </Menu.Item> */}
-                <Link to="/">
-                    <Menu.Item as='a'
-                        style={{ marginTop: `125%` }}
-                    >
-                        <Icon name='sun' />
-                        Zachary
-            </Menu.Item>
-                </Link>
-                <Link to="/education/">
-                    <Menu.Item as='a'>
-                        <Icon name='pencil' />
-                        Education
-            </Menu.Item>
-                </Link>
-                <Link to="/experience/">
-                    <Menu.Item as='a'>
-                        <Icon name='cog' />
-                        Experience
-            </Menu.Item>
-                </Link>
-                <Link to="/projects/">
-                    <Menu.Item as='a'>
-                        <Icon name='code' />
-                        Projects
-            </Menu.Item>
-                </Link>
-                <Menu.Item
-                    style={{
-                        marginTop: `50%`,
-                        padding: `15px`,
-                        bottom: `5px`
-                    }}
+            <Link to="/">
+            <Menu.Item as='a'
+                style={{ marginTop: `125%` }}
                 >
-                    Zachary Ellison © {new Date().getFullYear()}
-                </Menu.Item>
+                <Icon name='sun' />
+                Zachary
+            </Menu.Item>
+            </Link>
+            <Link to="/education/">
+            <Menu.Item as='a'>
+                <Icon name='pencil' />
+                Education
+            </Menu.Item>
+            </Link>
+            <Link to="/experience/">
+                <Menu.Item as='a'>
+                    <Icon name='cog' />
+                    Experience
+            </Menu.Item>
+            </Link>
+            <Link to="/projects/">
+                <Menu.Item as='a'>
+                    <Icon name='code' />
+                    Projects
+            </Menu.Item>
+            </Link>
+            <Menu.Item
+                style={{
+                    marginTop: `50%`,
+                    padding: `15px`,
+                    bottom: `5px`
+                }}
+            >
+                Zachary Ellison © {new Date().getFullYear()}
+            </Menu.Item>
             </Sidebar>
         </div>
     )
